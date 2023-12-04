ccc7e9e0946b7b0e0a08427e42be18013c99c8c8
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2023-12-04 20:42:12 +0700

    Add shop

---
+++ shop/src/components/shop/Pancake.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Pancake() {
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold title">Please be back soon ! (We make thing better)</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Pancake
---
+++ shop/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        vamvam: {
+          "primary": "#dc2626",
+          "secondary": "#ef4444",
+          "accent": "#b91c1c",
+          "neutral": "#dc2626",
+          "base-100": "#fee2e2",
+          "info": "#22d3ee",
+          "success": "#84cc16",
+          "warning": "#facc15",
+          "error": "#991b1b",
+        },
+      },
+      "light",
+      "dark",
+    ],
+  },
+}
