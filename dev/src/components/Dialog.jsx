abefd277b684cc79d3dafa84b38718847324647a
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2023-12-05 19:58:58 +0700

    Update info

--- dev/src/components/Dialog.jsx
+++ dev/src/components/Dialog.jsx
@@ -6,7 +6,7 @@ function Dialog() {
             <dialog id="contract-model" className="modal">
                 <div className="modal-box">
                     <h3 className="font-bold text-lg">Contract me</h3>
-                    <p className="py-4">Discord : @fujatyping (Fujatyping#0148)<br />Twitter (X) : @fujatyping<br />Mail : me@fujatyping.dev</p>
+                    <p className="py-4">Discord : fujatyping (Fujatyping#0148)<br />Twitter (X) : @fujatyping<br />Mail : me@fujatyping.dev</p>
                 </div>
                 <form method="dialog" className="modal-backdrop">
                     <button>close</button>
