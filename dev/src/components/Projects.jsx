721f9cf3327fe10902626f6530811b74507e5e7e
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2023-12-05 18:25:32 +0700

    Add project

--- dev/src/components/Projects.jsx
+++ dev/src/components/Projects.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+/* Assets */
+import Pancake from '../assets/Screenshot/Pancake.png'
+import HfA from '../assets/Screenshot/HfA-Project.webp'
+import Yw64 from '../assets/Screenshot/Yw64Sl10.png'
+
 function Projects() {
   return (
     <>
       <div className="card w-50 bg-base-100 shadow-xl projects">
-        <figure><img src="https://images-ext-1.discordapp.net/external/wvAQ5uqSIlwwge-OhZVCDPL9VHLpnhGieA4oMIKi-3k/https/opengraph.githubassets.com/b34dd612be331381f840a2ca7f80fc49deb3b88179532a0ac877007860bad0a7/FujaTyping/HfA-Project?format=webp&width=965&height=483" alt="Shoes" /></figure>
+        <figure><img src={Pancake} alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title"><b>Meao แพนเค้ก</b></h2>
+          <p className='card-details' >แพนเค้ก " Meao แพนเค้ก " แพนเค้กโฮมเมด อบสดใหม่ทุกวัน ให้คุณได้สัมผัสรสชาติที่หอมกรุ่น อร่อยไม่เหมือนใคร</p>
+          <div className="card-actions justify-end">
+            <a href='https://shop.fujatyping.dev/pancake' className="btn btn-primary">View project page <span className="material-symbols-outlined">captive_portal</span></a>
+          </div>
+        </div>
+      </div>
+      <div className="card w-50 bg-base-100 shadow-xl projects">
+        <figure><img src={HfA} alt="Shoes" /></figure>
         <div className="card-body">
           <h2 className="card-title"><b>HfA-Project</b></h2>
           <p className='card-details' >Smart home from arduino UNO</p>
@@ -14,7 +29,7 @@ function Projects() {
         </div>
       </div>
       <div className="card w-50 bg-base-100 shadow-xl">
-        <figure><img src="https://camo.githubusercontent.com/6ee13273f94ed9d88dd0528a83b72ba0ce34e0c1b2d6684eba023e5d64ac8d94/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313037313430313438353233393333323836342f313137373934343230363731353637303633382f42616e6e65722e706e67" alt="Shoes" /></figure>
+        <figure><img src={Yw64} alt="Shoes" /></figure>
         <div className="card-body">
           <h2 className="card-title">Yorwor64Slash10-Project</h2>
           <p className='card-details' >Web App ของ ห้อง 3/10! โครงการโอเพ่นซอร์สนี้ออกแบบมาเพื่อมอบแพลตฟอร์มที่สะดวกและมีประสิทธิภาพสำหรับการจัดการกิจกรรมในชั้นเรียน</p>
