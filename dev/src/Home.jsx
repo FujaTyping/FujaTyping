fd68b4423a5ed68cb1f7fa3c0edb5367f00cf85e
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2023-12-31 18:49:36 +0700

    Add more effect

--- dev/src/Home.jsx
+++ dev/src/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './App.css'
 import './index.css'
 import 'animate.css';
+import Typed from 'typed.js';
+import { RandomReveal } from 'react-random-reveal'
 
 /* Components */
 import Footer from './components/Footer'
@@ -14,13 +16,31 @@ import icon from './assets/icon.png'
 import dividerSec from './assets/Quote-divider.png'
 
 function Home() {
+    const QuoteTyping = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(QuoteTyping.current, {
+            strings: ['" Any fool can write code that a computer can understand. Good programmers write code that humans can understand. "', '" Experience is the name everyone gives to their mistakes. "', `" 1 st rule of programming : If it works, don't touch it. "`],
+            loop: true,
+            showCursor: true,
+            cursorChar: ' |',
+            typeSpeed: 30,
+            backDelay: 5000,
+            backSpeed: 50,
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
     return (
         <>
             <div className="hero min-h-screen bg-base-200 headder animate__animated animate__fadeIn animate__delay-0.5s">
                 <div className="hero-content flex-col lg:flex-row">
                     <img src={icon} className="max-w-sm shadow-2xl rounded-full profile" /><br />
                     <div>
-                        <h1 className="text-5xl font-bold title">Siraphop Sukchu</h1>
+                        <h1 className="text-5xl font-bold title"><RandomReveal isPlaying duration={3} characters="Siraphop Sukchu" /></h1>
                         <p className="py-6">Hi ! , I'm <b>Siraphop Sukchu</b> you can simply call me <b>Fujatyping</b></p>
                         <div className="join">
                             <a href='https://project.fujatyping.dev/' className="btn join-item btn-outline btn-primary"><span className="material-symbols-outlined">tactic</span>My project</a>
@@ -55,7 +75,7 @@ function Home() {
                 <div className="hero-content text-center">
                     <div className="max-w-md">
                         <h1 className="text-5xl font-bold title">Quotes</h1>
-                        <p style={{ fontFamily: "Pacifico" }} className="py-6">" Any fool can write code that a computer can understand. Good programmers write code that humans can understand "</p>
+                        <p style={{ fontFamily: "Pacifico" }} className="py-6"><span style={{ fontFamily: "Pacifico" }} ref={QuoteTyping}></span></p>
                     </div>
                 </div>
             </div>
